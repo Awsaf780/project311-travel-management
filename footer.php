a53942fce41219524dae3ade3ed1c31727785ed2
Awsaf Mahmood <awsaf780@gmail.com>
2020-04-05 20:01:00 +0600

    Added Footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <div class="footer">
+		<div id="separator">
+			<div class="foot1">
+				<div class="link"><a href=""><img src="images/facebook.svg"></a></div>
+				<div class="link"><a href=""><img src="images/linkedin.svg"></a></div>
+			</div>
+			<div class="foot2">
+				<div class="link"><a href="terms.php">Terms And Conditions</a></div>
+				<div class="link"><a href="privacy.php">Privacy Policy</a></div>
+			</div>
+		</div>
+
+		<div class="foot3">
+			<div class="link"><a>Copyright 2020 CSE 311 Project</a></div>
+		</div>
+	</div>
+  </body>
+</html>
