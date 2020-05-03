db3637611d305e4592e18c68466b2c77129a75a0
ravina <ravinajaved02@gmail.com>
2020-05-03 16:48:59 +0600

    booking history added

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 
   include("header.php");
 
-  $your_value = "1234567890";
+
 $rj=mysqli_query($conn, "select * from client where client.username= '$login_session' ");
 $abc = mysqli_fetch_assoc($rj);
 $bbc =  $abc['email'];
@@ -28,7 +28,7 @@ $ebc=$abc['fullname'];
 
     <div class="display-pic">
       <img style="max-height: 20vh" src='<?php echo $profile_pic; ?>' >
-      <h1 style="color: grey"><?php echo $ebc ?></h1>
+      <h1 style="color: black"><?php echo $ebc ?></h1>
 
     </div>
 
@@ -53,11 +53,71 @@ $ebc=$abc['fullname'];
   </div>
 
   <div class="feature-booking">
-    <h3>Add Booking History here .. Maybe using tables</h3>
+
+    <?php
+    $serial=1;
+$booking_result=mysqli_query($conn, "SELECT package.name AS Package,hotel.name AS Hotel,transport.name AS Transport,package.cost AS Amount,booking.travel_date
+
+FROM booking LEFT OUTER JOIN (hotel,package,transport)
+
+ON booking.hotel_id= hotel.id  AND booking.package_id=package.id AND booking.transport_id=transport.id
+
+WHERE booking.client_id=(SELECT client.id
+
+FROM client
+
+WHERE username= '$login_session')" );
+
+
+      ?>
+  </div>
+  <!DOCTYPE html>
+  <html>
+  <head>
+
+  </head>
+  <body>
+
+  <div>
+
   </div>
 
+
+
+  	<div class="view-box">
+
+  		<div class="package-text">
+  			<h1 style="color: black"><?php echo 'Booking History';?></h1>
+        <table>
+  				<tr>
+            <th>Serial</th>
+  					<th>Package</th>
+            <th>Hotel</th>
+            <th>Transport</th>
+            <th>Package Cost</th>
+            <th>Travel Date</th>
+
+  				</tr>
+<?php
+if(mysqli_num_rows($booking_result) > 0)
+
+{while ($gogo=mysqli_fetch_assoc($booking_result))
+  { echo " <tr>
+    <td>".$serial++."</td>
+    <td>".$gogo['Package']."</td>
+    <td>".$gogo['Hotel']."</td>
+    <td>".$gogo['Transport']."</td>
+    <td>".$gogo['Amount']."</td>
+    <td>".$gogo['travel_date']."</td>
+    </tr> <br> ";}}
+?>
+
+  			</table>
+  		</div>
+    </body>
+  </html>
   <div class="feature-booking">
-    <h3>Add Transaction History maybe?</h3>
+    <h3>Transaction History</h3>
   </div>
 
 
