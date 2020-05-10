5a5c4e38c09568a7467ace42464ef74cb7d1caa6
ravina <ravinajaved02@gmail.com>
2020-05-10 12:29:12 +0600

    booking id error fixed

--- insert.php
+++ insert.php
@@ -24,7 +24,15 @@
   $finding_count=mysqli_query($conn,"select id from booking ORDER BY id DESC LIMIT 1");
   $finding_count_2=mysqli_fetch_assoc($finding_count);
   $finding_count_3=$finding_count_2['id'];
-  $finding_count_final=$finding_count_3+1;
+  if(is_null($finding_count_3))
+  {
+    $finding_count_final=1;
+  }
+  else
+  {
+    $finding_count_final=$finding_count_3+1;
+  }
+
 
 
   ?>
